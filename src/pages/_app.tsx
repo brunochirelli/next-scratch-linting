759c8b1dbbc6edb45367df8cdf72aa4305ff7d6b
brunochirelli <brunochirelli@gmail.com>
2021-06-24 14:40:54 -0300

    add styled-components theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { theme } from "../themes/themes";
+
+import "../static/styles/normalize.css";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,3 @@ import styled from "styled-components";
 export default function Home() {
   return <div>Oie</div>;
 }
-
-const Title = styled.h1`
-  position: relative;
-  top: -2rem;
-  width: 5rem;
-  height: 10rem;
-  color: "red";
-  text-decoration: underline;
-  border: 1px solid green;
-
-  .classe {
-    position: absolute;
-  }
-`;
