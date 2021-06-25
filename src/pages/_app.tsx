8fc6bf55f6ea32182943c82f35e7dcc6beb43686
brunochirelli <brunochirelli@gmail.com>
2021-06-25 12:45:39 -0300

    add normalize e global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
+import { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
+
 import { theme } from "../themes/themes";
 
-import "../static/styles/normalize.css";
+import "../styles/normalize.css";
+import "../styles/global.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
